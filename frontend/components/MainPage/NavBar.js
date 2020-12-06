8df88485e0b468f76d9d7d678ec4120befcac1eb
Collin Eng <eng.engineereng@gmail.com>
2020-12-06 14:13:03 -0800

    wip

---
+++ frontend/components/MainPage/NavBar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class NavBar extends Component {
+    foo = [];
+    render() {
+        return (
+        <>
+          
+        </>
+        );
+    } 
+}
+export default Sidebar;
---
+++ frontend/components/MainPage/Sidebar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class Sidebar extends Component {
+    foo = [];
+    render() {
+        return (
+        <>
+          
+        </>
+        );
+    } 
+}
+export default Sidebar;
